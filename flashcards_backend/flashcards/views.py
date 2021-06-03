cb62f1a55719b6e550e0afdc647dd8251aa8a49e
nmar30 <nmarcisso@live.com>
2021-06-03 15:22:24 -0400

    Added logic to prevent retrieving/updating/deleting flashcards with an incorrect collection id in the url

--- flashcards_backend/flashcards/views.py
+++ flashcards_backend/flashcards/views.py
@@ -18,7 +18,7 @@ class FlashcardList(APIView):
         serializer = FlashcardSerializer(flashcard, many=True)
         return Response(serializer.data)
 
-    def post(self, request):
+    def post(self, request, collection):
         serializer = FlashcardSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -27,27 +27,27 @@ class FlashcardList(APIView):
 
 
 class FlashcardDetails(APIView):
-    def get_object(self, pk):
+    def get_object(self, collection, pk):
         try:
-            return Flashcard.objects.get(pk=pk)
+            return Flashcard.objects.get(collection=collection, pk=pk)
         except Flashcard.DoesNotExist:
             raise Http404
 
     def get(self, request, collection, pk):
-        flashcard = self.get_object(pk)
+        flashcard = self.get_object(collection, pk)
         serializer = FlashcardSerializer(flashcard)
         return Response(serializer.data)
 
-    def put(self, request, pk):
-        flashcard = self.get_object(pk)
+    def put(self, request, collection, pk):
+        flashcard = self.get_object(collection, pk)
         serializer = FlashcardSerializer(flashcard, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    def delete(self, request, pk):
-        flashcard = self.get_object(pk)
+    def delete(self, request, collection, pk):
+        flashcard = self.get_object(collection, pk)
         serializer = FlashcardSerializer(flashcard)
         flashcard.delete()
         return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)

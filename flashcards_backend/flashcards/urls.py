281e2781cd498c54e5dec1d7f00b146d811adc6b
nmar30 <nmarcisso@live.com>
2021-06-03 14:52:28 -0400

    Added FlashcardDetails View

--- flashcards_backend/flashcards/urls.py
+++ flashcards_backend/flashcards/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('collection/', views.CollectionList.as_view()),
     path('collection/<int:pk>', views.CollectionDetails.as_view()),
     path('collection/<int:collection>/flashcard/', views.FlashcardList.as_view()),
+    path('collection/<int:collection>/flashcard/<int:pk>', views.FlashcardDetails.as_view()),
 ]
--- flashcards_backend/flashcards/views.py
+++ flashcards_backend/flashcards/views.py
@@ -26,6 +26,33 @@ class FlashcardList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+class FlashcardDetails(APIView):
+    def get_object(self, pk):
+        try:
+            return Flashcard.objects.get(pk=pk)
+        except Flashcard.DoesNotExist:
+            raise Http404
+
+    def get(self, request, collection, pk):
+        flashcard = self.get_object(pk)
+        serializer = FlashcardSerializer(flashcard)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        flashcard = self.get_object(pk)
+        serializer = FlashcardSerializer(flashcard, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        flashcard = self.get_object(pk)
+        serializer = FlashcardSerializer(flashcard)
+        flashcard.delete()
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+
+
 class CollectionList(APIView):
     def get(self, request):
         collection = Collection.objects.all()
@@ -38,6 +65,7 @@ class CollectionList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 class CollectionDetails(APIView):
     def get_object(self, pk):
